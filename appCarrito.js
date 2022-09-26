44eece188e51ca4753d13f05cdd703ce3cd8b331
ignacio gaston odella <108488609+Nano92OB@users.noreply.github.com>
2022-09-26 12:55:48 -0300

    carrito de compras terminado

--- appCarrito.js
+++ appCarrito.js
@@ -10,6 +10,10 @@ let carrito = {}
 
 document.addEventListener("DOMContentLoaded", (e) => {
   fetchData();
+  if(localStorage.getItem('carrito')){
+    carrito = JSON.parse(localStorage.getItem('carrito'))
+    pintarCarrito()
+  }
 });
 
 cards.addEventListener('click', (e) =>{
@@ -80,6 +84,8 @@ const pintarCarrito = () =>{
   items.appendChild(fragment)
 
   pintarFooter()
+
+  localStorage.setItem('carrito', JSON.stringify(carrito))
 }
 
 const pintarFooter = () =>{
